32450e089ab8c6a2a5e675d8ea9b781cefd0cbfe
Никола Вукосављевић <hauzer.nv@gmail.com>
2018-04-10 20:54:57 +0200

    Преводи, побољшања.
    У именима карата: "напад" -> "ударац" како не би било конфузије око типа
    карата "Напад", и карата које садрже "напад" у имену (сада садрже
    "ударац").
    Мало побољшана скрипта за пребацивање из ћирилице у латиницу.
    Одрађено још превода у events.json.

--- make-srb-lat.py
+++ make-srb-lat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import subprocess
 
 
@@ -8,9 +9,10 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 srb_cyr_dir = os.path.join(root_dir, 'srb-cyr')
 srb_lat_dir = os.path.join(root_dir, 'srb-lat')
 
-os.makedirs(srb_lat_dir, exist_ok=True)
-_, _, files = next(os.walk(srb_cyr_dir))
+shutil.rmtree(srb_lat_dir, ignore_errors=True)
+os.makedirs(srb_lat_dir)
 
+_, _, files = next(os.walk(srb_cyr_dir))
 for f in files:
     with open(os.path.join(srb_cyr_dir, f), 'rb') as cyr, \
             open(os.path.join(srb_lat_dir, f), 'wb') as lat:

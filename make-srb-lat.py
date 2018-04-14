6e588c5c237dbe638b96fbc259854c7db8a03b60
Никола Вукосављевић <hauzer.nv@gmail.com>
2018-04-14 17:23:14 +0200

    Исправке, ажурирање.
    Исправке форматирања описа карата.
    Побољшани описи догађаја.
    Њов -> Њова

--- make-srb-lat.py
+++ make-srb-lat.py
@@ -14,7 +14,14 @@ os.makedirs(srb_lat_dir)
 
 _, _, files = next(os.walk(srb_cyr_dir))
 for f in files:
-    with open(os.path.join(srb_cyr_dir, f), 'rb') as cyr, \
-            open(os.path.join(srb_lat_dir, f), 'wb') as lat:
-        ret = subprocess.run(['recode-sr-latin'], input=cyr.read(), stdout=subprocess.PIPE)
-        lat.write(ret.stdout)
+    with open(os.path.join(srb_cyr_dir, f), 'r') as cyr, \
+            open(os.path.join(srb_lat_dir, f), 'w') as lat:
+        ret = subprocess.run(['recode-sr-latin'], input=cyr.read().encode(), stdout=subprocess.PIPE)
+        data = ret.stdout.decode()
+
+        if f == 'ui.json':
+            data = data.replace(
+                '"Tajlandski",\n{}"Srpski"'.format(' ' * 6),
+                '"Tajlandski",\n{}"Српски"'.format(' ' * 6)
+            )
+        lat.write(data)
